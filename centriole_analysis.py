b146abd24f14e25c33ad63fbdad3fc202197e827
Martin Schorb <martin.schorb@embl.de>
2023-04-19 16:29:51 +0200

    centriole crops

--- centriole_analysis.py
+++ centriole_analysis.py
@@ -85,25 +85,13 @@ def cropvals(v1,v2,ax, minwindow=0.3,pxs=0.1):
 
 strain = sys.argv[1] #'B-CLL'
 
-
-indir = './Tabellen'
+indir = './analysis'
 datadir = './data/tomo/'
-imagedir = os.path.join(datadir,'images','bdv-n5')
-joindir = '../Tomography/joined/'
-
-xmldir = 'xml_orig'
-
-
-patientjson = './patients.json'
-
-ndigits = 2
+imagedir = os.path.join(datadir,'images','ome-zarr')
 
 overwr_source = False
 
-
-
-
-a=glob.glob(os.path.join(indir,strain,'*'))
+a=glob.glob(os.path.join(indir,strain,'*xlsx'))
 
 # if len(a) > 1:
 #     raise IndexError('more than one file found')
@@ -117,42 +105,17 @@ badfiles = False
 
 for xlfile in a:
     skip=False
-    with open(patientjson) as f:
-        patients = json.load(f)
+
 # xlfile = a[0]
     wb = load_workbook(xlfile)
     pxs = 0.0015578000068664551
-    
-    
+
     if not 'data' in wb.sheetnames: continue
     
     sheet = wb['data']
     
-    
-    # parse patient
-    
-    patient = os.path.basename(xlfile).split('_')[0]
-    
-    # anonymize
-    entity_pat = dict(filter(lambda elem: strain in elem[1], patients.items()))
-    
-    if patient in entity_pat.keys():
-        newid = entity_pat[patient]
-    else: 
-        if len(entity_pat)>0:
-            last_id = list(entity_pat.values())[-1]
-            newid = strain + '_' + str(int(last_id.split('_')[1])+1).zfill(ndigits)
-        else:
-            newid = strain + '_' + '0'.zfill(ndigits)
-    
-    patients[patient] = newid 
-
-    
-    
     # parse file ID
-    
-    filesin = [cell.value for cell in sheet['B'][1:]]
-    
+
     # parse positions
     
     pcells = np.array(sheet['I:L'])[:,1:]
@@ -163,152 +126,156 @@ for xlfile in a:
     
     c_id = [cell.value for cell in sheet['F'][1:]]
 
-    
+    t_id = [cell.value for cell in sheet['A'][1:]]
+
     # extract unique targets
     
     targets=[]
-        
-    for infile in np.unique(filesin):
-        print('Processing '+infile+'. Patient: '+patient+'   ...\n')
-        
-        
-        grid = infile.partition('_')[0]
-        
-        
-        tomoid = infile.partition('_')[2]
-        
-        newxml = os.path.join(imagedir,newid+'_'+infile+'.xml') 
-        # with open(newxml, 'r') as f: xmltxt = f.readlines()
-        
-        if os.path.exists(newxml):
-                
-            
-            tree = ET.parse(newxml)
-            
-            root = tree.getroot()
-                    
-            for child in root:
-                if child.tag == 'SequenceDescription':
-                    for sqch in child:
-                        if sqch.tag == 'ViewSetups':                        
-                            for vsch in sqch[0]:
-                                
-                                if vsch.tag=='size':
-                                    imsize = vsch.text
-                                    
-                                    
-            im_sz = list(map(int,imsize.split(' ')))
-        
-        # assemble join file path
-        filepath= tomoid + '.join'
-            
-        sourcename = newid+'_'+infile
-        
-                    
-        if overwr_source:
-            
-            
-            
-            
-            mrc_in = os.path.join(joindir,strain, patient,grid,filepath)
-    
-            try:
-                mfile = mrc.mmap(mrc_in,permissive = 'True')
-            except:
-                print('Error in '+mrc_in)
-                badfiles = True
-                continue
-            
-            pxs = mfile.voxel_size.x / 10000 # in um
-            
-            im_sz = [mfile.header.nx,mfile.header.ny,mfile.header.nz]
-            
-            n5link = '_'+patient+'_'+infile
-            
-            n5file = os.path.join(imagedir,strain,n5link+'.n5')        
-                    
 
-            newlink = os.path.join(strain,sourcename)
-            newn5 = os.path.join(imagedir,newlink+'.n5')
-            
-            if not os.path.exists(newn5):
-                if not os.path.exists(n5file):
-                    print('adding '+infile+ ' to list of files to convert.')
-                    joinfiles.append(patient+'/'+infile)
-                    skip = True
-                    continue
-                
-                # move n5
-                shutil.move(n5file,newn5)
-            
-    
-            
-            # change link in XML
-            xmlfile = os.path.join(imagedir,strain,'_'+patient+'_'+infile+'.xml')       
-            
-            if not os.path.exists(xmlfile):
-                xmlfile = os.path.join(xmldir,'_'+patient+'_'+infile+'.xml')
-            
-                  
-            with open(xmlfile, 'r') as f: xmltxt = f.read()
-            
-            
-            xmltxt = xmltxt.replace(n5link,newlink)
-            xmltxt = xmltxt.replace(tomoid+'<',sourcename+'<')
-            #remove original file reference
-            
-            xmltxt = re.sub('\<OriginalFile\>.*\<\/OriginalFile\>\\n','',xmltxt)
-            
-            with open(newxml, 'w') as f: f.write(xmltxt)
-    
-            # os.remove(xmlfile)
-            
-            
-            
-            
-            # add source to dataset        
-            
-            # generate tomo view
-            
-            
-            
-            disp = mm.view_metadata.get_image_display(sourcename,[sourcename])      
-            
-    
-            view = mm.get_view(names = [sourcename],
-                                source_types = ['image'],
-                                sources = [[sourcename]],
-                                display_settings = [disp],
-                                is_exclusive = True,
-                                menu_name = 'Tomograms' 
-                                )       
-            
-            mm.add_source_to_dataset(dataset_folder = datadir,
-                                   source_type = 'image',
-                                   source_name = sourcename,
-                                   image_metadata_path = newxml,
-                                   view = view,
-                                   overwrite = overwr_source
-                                   )
-            
-            del(mfile)
-            
-            
-                
+    filesin = glob.glob(os.path.join(imagedir,strain + '*'))
+
+
+    for infile in np.unique(filesin):
+
+        print('Adding centriole crops for tomogram ' + infile + '.\n......')
         
+        grid = infile.split('_')[2]
+
+        tomoid = infile.split('_')[3].split('.')[0]
+        
+        # # with open(newxml, 'r') as f: xmltxt = f.readlines()
+        #
+        # if os.path.exists(newxml):
+        #
+        #
+        #     tree = ET.parse(newxml)
+        #
+        #     root = tree.getroot()
+        #
+        #     for child in root:
+        #         if child.tag == 'SequenceDescription':
+        #             for sqch in child:
+        #                 if sqch.tag == 'ViewSetups':
+        #                     for vsch in sqch[0]:
+        #
+        #                         if vsch.tag=='size':
+        #                             imsize = vsch.text
+        #
+        #
+        #     im_sz = list(map(int,imsize.split(' ')))
+        #
+        # # assemble join file path
+        # filepath= tomoid + '.join'
+        #
+        # sourcename = newid+'_'+infile
+        #
+        #
+        # if overwr_source:
+        #
+        #
+        #
+        #
+        #     mrc_in = os.path.join(joindir,strain, patient,grid,filepath)
+        #
+        #     try:
+        #         mfile = mrc.mmap(mrc_in,permissive = 'True')
+        #     except:
+        #         print('Error in '+mrc_in)
+        #         badfiles = True
+        #         continue
+        #
+        #     pxs = mfile.voxel_size.x / 10000 # in um
+        #
+
+        with open(os.path.join(infile, 's0','.zarray')) as f:
+            im_md = json.load(f)
+
+        im_sz = im_md['shape']
+
+
+        #
+        #     n5link = '_'+patient+'_'+infile
+        #
+        #     n5file = os.path.join(imagedir,strain,n5link+'.n5')
+        #
+        #
+        #     newlink = os.path.join(strain,sourcename)
+        #     newn5 = os.path.join(imagedir,newlink+'.n5')
+        #
+        #     if not os.path.exists(newn5):
+        #         if not os.path.exists(n5file):
+        #             print('adding '+infile+ ' to list of files to convert.')
+        #             joinfiles.append(patient+'/'+infile)
+        #             skip = True
+        #             continue
+        #
+        #         # move n5
+        #         shutil.move(n5file,newn5)
+        #
+        #
+        #
+        #     # change link in XML
+        #     xmlfile = os.path.join(imagedir,strain,'_'+patient+'_'+infile+'.xml')
+        #
+        #     if not os.path.exists(xmlfile):
+        #         xmlfile = os.path.join(xmldir,'_'+patient+'_'+infile+'.xml')
+        #
+        #
+        #     with open(xmlfile, 'r') as f: xmltxt = f.read()
+        #
+        #
+        #     xmltxt = xmltxt.replace(n5link,newlink)
+        #     xmltxt = xmltxt.replace(tomoid+'<',sourcename+'<')
+        #     #remove original file reference
+        #
+        #     xmltxt = re.sub('\<OriginalFile\>.*\<\/OriginalFile\>\\n','',xmltxt)
+        #
+        #     with open(newxml, 'w') as f: f.write(xmltxt)
+        #
+        #     # os.remove(xmlfile)
+        #
+        #
+        #
+        #
+        #     # add source to dataset
+        #
+        #     # generate tomo view
+        #
+        #
+        #
+        #     disp = mm.view_metadata.get_image_display(sourcename,[sourcename])
+        #
+        #
+        #     view = mm.get_view(names = [sourcename],
+        #                         source_types = ['image'],
+        #                         sources = [[sourcename]],
+        #                         display_settings = [disp],
+        #                         is_exclusive = True,
+        #                         menu_name = 'Tomograms'
+        #                         )
+        #
+        #     mm.add_source_to_dataset(dataset_folder = datadir,
+        #                            source_type = 'image',
+        #                            source_name = sourcename,
+        #                            image_metadata_path = newxml,
+        #                            view = view,
+        #                            overwrite = overwr_source
+        #                            )
+        #
+        #     del(mfile)
+        #
+        #
+        #
+        #
         # find corresponding rows in data
-        filerows = [i for i,item in enumerate(filesin) if item == infile]
+        filerows = [i for i,item in enumerate(t_id) if item == tomoid]
     
         filelabels = [c_id[i] for i in filerows]
     
         lenrows = [item for item in filerows if 'length' in c_id[item]]
     
         numcent = np.unique([c_id[i] for i in lenrows])
-        
-        
-        
 
-    
         for cent in numcent:
             
             
@@ -332,18 +299,16 @@ for xlfile in a:
             # midpts = [filerows[i] for i,label in enumerate(filelabels) if (cent.partition('.')[0] in label) & ('diam.mid' in label)]
     
             # midpos = np.array((x[midpts[0]],y[midpts[0]],z[midpts[0]]))
-    
-    
-            t['file']=filepath
-            t['lpt']=lenpos
-            # t['midpt']=midpos   
-            
+
+            # t['file']=filepath
+            # t['lpt']=lenpos
+            # t['midpt']=midpos
+
+            sourcename = os.path.basename(infile).rstrip('.ome.zarr')
     
             trafo,cropmin,cropmax = cropvals(lenpos[0,:],lenpos[1,:],[1,0,0],minwindow=1,pxs=pxs)
-
             
-            targets.append(t)
-            
+            # targets.append(t)
             
             centname = sourcename + '_' + cent.split('.')[0]
             
@@ -397,29 +362,13 @@ for xlfile in a:
             mm.add_view_to_dataset(dataset_folder = datadir,                                       
                                    view_name = centname + "_crop",
                                    view = cview)
-            
-            
-   
-    if not skip:   
-        with open(patientjson,'w') as f:
-            patients = json.dump(patients,f, indent=4, sort_keys=True)
+
     
 if not badfiles:
     
     with open(strain+'_joinfiles.txt','w') as f:
        f.write('\n'.join(joinfiles))
-            
-            
-            
-            
-    
 
-
-    
-    
-    
-    
-    
             # normvec = np.cross(np.diff(lenpos,axis=0),lenpos[0]-midpos)
             # normvec = normvec/np.linalg.norm(normvec)
             # v1 = np.cross(normvec,[0,0,1])
