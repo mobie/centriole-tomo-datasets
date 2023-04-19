d2607126b802522596a43a6a6cd8a90457a6c78d
Martin Schorb <martin.schorb@embl.de>
2023-04-19 11:42:28 +0200

    nw conversion script

--- join2bdv.py
+++ join2bdv.py
@@ -6,131 +6,95 @@ Created on Mon Jun  7 08:27:24 2021
 @author: schorb
 """
 
-import numpy as np
-import pyEM as em
-
-#import pybdv
-from pybdv import transformations as tf
 import mrcfile as mrc
-import glob
-import re
+import random
+import time
 import sys
 import os
-
-import time
-import shutil
-
-from skimage import io
-
-import bdv_tools as bdv
+import mobie
+import glob
+from multiprocessing import Pool
 
 
-cluster = True
+target='slurm'
 
+outformat='ome.zarr'
+chunks = list((64,128,128))
+downscale_factors = list(([2,2,2],[2,2,2],[1,2,2],[1,2,2],[2,2,2],[2,2,2]))
 
-bdv_unit = 'um'
-zstretch = 1
-timept = 0
-
-submit_interval = 2 # time to wait between submitting jobs to not overload the group share IO
-blocksize = 8
-break_interval = 40
-
-
-outformat='.n5'
-
-chunks = list((32,192,192))
-downscale_factors = list(([1,2,2],[1,2,2],[1,2,2],[1,2,2],[1,4,4]))
-
-indir = '/g/schwab/Tobias/Tomography/joined/'
+indir = '/g/schwab/koehrer/EMPIAR/volumes'
 suffix = sys.argv[1]
 
+# indir = os.path.join(indir,suffix)
 
-indir = os.path.join(indir,suffix)
+# fileslist = os.path.join('/g/schwab/Tobias/MoBIE/',suffix+'_joinfiles.txt')
+joinlist = glob.glob(os.path.join(indir, suffix.split('_')[0], suffix+'*'))
 
-fileslist = os.path.join('/g/schwab/Tobias/MoBIE/',suffix+'_joinfiles.txt')
+print('Found ' + str(len(joinlist)) + ' files for type ' + suffix + '.')
 
-outdir = os.path.join('/g/schwab/Tobias/MoBIE/data/tomo/images/bdv-n5',suffix)
-
-
-
-with open(fileslist,'r') as f:
-    joinlist=f.read().splitlines()
-
-
-
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
+outdir = '/g/emcf/schorb/code/centrioles-tomo-datasets/data'
 
 idx = 0
 
-for file in joinlist:
-    file_comp = file.split('_')
-    file =os.path.join(indir,file_comp[0],file_comp[1]+'.join')
-    
-    base = os.path.splitext(file.split(indir)[1])[0]
-        
-    outfile = os.path.join(outdir,re.sub('/','_',base))+outformat
-    
-    if os.path.exists(os.path.join(outdir,re.sub('/','_',base)+'.xml')):
-        print('Skipping '+base+'. It already exists.')
-        continue
+def mobieconvert(infile):
+    # skip empty lines
+    if len(infile)<3: return
 
-    if os.path.exists(outfile):
-        print('re-doing '+base+'.')
-        shutil.rmtree(outfile)
-        #continue
-    idx += 1
-    
-    if idx%blocksize == 0:
-        time.sleep(break_interval)
+    sleeptime = int(random.random()*90)
+
+    base = os.path.basename(infile).split('_join')[0]
+
+    # if os.path.exists(os.path.join(outdir,re.sub('/','_',base)+'.ome.zarr')):
+    #     print('Skipping '+base+'. It already exists.')
+    #     continue
+    #
+    # if os.path.exists(outfile):
+    #     print('re-doing '+base+'.')
+    #     shutil.rmtree(outfile)
+    #     #continue
+    # idx += 1
+    #
+    # if idx%blocksize == 0:
+    #     time.sleep(break_interval)
+    # else:
+    #     time.sleep(submit_interval)
+    # if not os.path.exists(os.path.join(outdir, 'tomo', 'images',
+    #                                    outformat.replace('.', '-').replace('.', '-'),
+    #                                    base + '.' + outformat, 's6')):
+    if not base in mobie.metadata.read_dataset_metadata(os.path.join(outdir, 'tomo'))['sources'].keys():
+
+        print('converting ' + base + ' into MoBIE format after waiting for '+str(sleeptime)+' seconds.')
+
+        time.sleep(sleeptime)
+
+        # get pixel size
+
+        mfile = mrc.mmap(infile, permissive='True')
+        tomopx = mfile.voxel_size.x / 10000  # in um
+        del (mfile)
+        resolution = [tomopx] * 3
+
+        mobie.add_image(infile,
+                    "data",
+                    outdir,
+                    "tomo",
+                    base,
+                    resolution,
+                    downscale_factors,
+                    chunks,
+                    file_format="ome.zarr",
+                    target=target,
+                    max_jobs=20,
+                    menu_name='tomograms',
+                    tmp_folder='/scratch/schorb/mobie/'+base,
+                    int_to_uint=True
+                    )
+
     else:
-        time.sleep(submit_interval)
-
-    print('converting '+base+' into BDV format.')
-    
-    # get pixel size
-
-    mfile = mrc.mmap(file,permissive = 'True')
-    tomopx = mfile.voxel_size.x / 10000 # in um
-    del(mfile)
-    mat=np.eye(4)*tomopx
-    mat[2,2] = tomopx*zstretch
-    mat[3,3] = 1
-    
-    tf_tr = tf.matrix_to_transformation(mat).tolist()
-
-    setup_id = 0
-
-    view=dict()
-
-    view['resolution'] = [tomopx,tomopx,tomopx*zstretch]
-    view['setup_id'] = setup_id
-    view['setup_name'] = os.path.basename(base)
-
-    view['OriginalFile'] = file
-
-    view['trafo'] = dict()
-    view['trafo']['Scaling'] = tf_tr
-
-    view['attributes'] = dict()
-
-    # view['attributes']['displaysettings'] = dict({'id':setup_id,'color':bdv.colors['W'],'isset':'true'})
-    # view['attributes']['displaysettings']['Projection_Mode'] = 'Average'
-
-    # data = mfile.data
+        print('Skipping ' + base + '. Already found converted data.')
 
 
-    # # check if volume is rotated
-    # if data.shape[0]/data.shape[1]>5:
-    #     data = np.swapaxes(data,0,1)
+with Pool(25) as p:
+    p.map(mobieconvert, joinlist)
 
-    # data0 = np.swapaxes(data,0,2)
-    # data1 = np.fliplr(data0)
-    # data2 = np.swapaxes(data1,0,2)
 
-   # print(outfile)
-    
-    
-    bdv.write_bdv(outfile,file,view,outf='.n5',blow_2d=zstretch,downscale_factors=downscale_factors,cluster=cluster,infile=file,chunks=chunks)
-    del(view)
